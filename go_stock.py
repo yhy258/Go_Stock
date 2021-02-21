a128bf7e20f3661af4856907d21974957b213f1d
beargrllys <js_7744@naver.com>
2021-02-21 23:03:21 +0900

    Secret Key delete

--- go_stock.py
+++ go_stock.py
@@ -1,7 +1,7 @@
 from slacker import Slacker
 
 # 내 api bot Token.
-slack = Slacker('xoxb-1775081175811-1768103723574-0rr5F0Nc7mG6lNIcgzoJq3J5')
+slack = Slacker('')
 
 # Send a message to #general channel
 # channel 이름, post.. post 부분에서 주가 부분을 넣어주면 되겠네.!
